eb45bb94880392ccc4b9767033c87836f0a8ef55
100saubagya <saubhagyamaurya11.com>
2023-06-13 18:23:42 +0530

    first commit

---
+++ practise.js/1.js
@@ -0,0 +1,26 @@
+// let a = 6
+// let b = 4
+// console.log(a==b ,a!=b ,a===b , a>b ,a<b && a==5 ); 
+// let age = 100;
+// if(age>10 && age<20){
+//     console.log("your age lie between10 and 20")
+// }
+//  else {
+//  console.log ("your age doesnt lies between 10 and 20"/)
+ 
+ let age = "20"
+ switch(age){
+    case 1 :
+     console.log("your  is age 10")
+     break
+     case 2 :
+     console.log("your is age 12")
+     break
+     case 3 :
+     console.log("your is age 15")
+     break
+     case 4 :
+     console.log("your  is age 20")
+    default:
+     console.log("your age doesnot belog here")
+    }
---
+++ practise.js/qqq.js
@@ -0,0 +1,22 @@
+//q1///
+let a = "harry"
+let b = 6
+console.log(a+b)
+//q2//
+let c = {
+  name:"saubhagya"
+}
+c['friend'] = "shubham"
+c['rahul'] ="soanam"
+ console.log(c)
+
+ //q55//
+ const dict ={
+    appreciated:"ckackjbckjabckabjck",
+    bcsbcksbc:"cscbsbcks",
+    cscbsjhvcjhsvcskjb:"cshcjsvc",
+    cscbscvjshc:"cjcvsjcvwc",
+    smcscjsvcsjvcsvcnbkcjbsjkb:"csjcvjsvcjsc",
+}
+console.log(dict.bcsbcksbc)
+ 
